b80f6d2569ec8b3d255653a01b2db0857897e29d
ruby 33 <ruby.33@metralha.cec.ime.usp.br>
2017-01-27 21:49:27 -0200

    quase acabando só a falta a última aula

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,15 @@
 class Movie < ActiveRecord::Base
-  validates :name, length: {minimum: 2}
-  validates :bio, length: {maximum: 1000,
-    too_long: "%{count} characteres is the maximum allowed"}
-  validates :passoword, length: {in: 6..20}
-  validates :registration_number, length: {is: 6}
+  #validates :name, length: {minimum: 2}
+  #validates :bio, length: {maximum: 1000,
+  #  too_long: "%{count} characteres is the maximum allowed"}
+  #validates :passoword, length: {in: 6..20}
+  #validates :registration_number, length: {is: 6}
+  #validates :title, presence: true
+  #validates :title, uniqueness: true
+
   validates :title, presence: true
-  validates :title, uniqueness: true
+  validates :title,  uniqueness:{ scope: :release_date,
+    message: "Should not have two movies with same name and release_date" }
+
+    belongs_to :director, class_name: "Director", foreign_key: "director_id"
 end
